f10cbfa12edce95fd5a89064666c36b10f36e687
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-06-18 17:37:41 +0000

    in progress

--- src/js/app.js
+++ src/js/app.js
@@ -30,13 +30,15 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
   const name = variables.name == null ? "" : variables.name;
   const lastName = variables.lastname == null ? "" : variables.lastname;
+  const country = variables.country == null ? "" : variables.country;
+  const city = variables.city == null ? "" : variables.city;
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${name} ${lastName}</h1>
           <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h3>${city}, ${country}</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
